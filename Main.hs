bf99e80bc445391fb252d900b661df84495b64d6
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2017-04-11 22:32:51 +0200

    keep env in state

--- Main.hs
+++ Main.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Data.String
-import Control.Monad.Trans.State
+import Control.Monad.Trans.State.Strict
+-- import Control.Monad.State.Class
+-- import Control.Monad.Trans.RWS
+-- import Control.Monad.State
 import Control.Monad.Except
 import Data.List
 import System.Directory
@@ -23,7 +29,11 @@ import qualified TypeChecker as TC
 import qualified TT as C
 import qualified Eval as E
 import Pretty
-type Interpreter a = InputT IO a
+type Loader = StateT InterpState IO
+data InterpState = IS {mkEnv :: TC.TEnv -> TC.TEnv, names :: [String], modules :: C.Modules}
+initState :: InterpState
+initState = IS id [] []
+type Interpreter a = InputT Loader a
 
 -- Flag handling
 data Flag = Debug | Help | Version
@@ -51,69 +61,73 @@ showTree tree = do
 searchFunc :: [String] -> String -> [Completion]
 searchFunc ns str = map simpleCompletion $ filter (str `isPrefixOf`) ns
 
-settings :: [String] -> Settings IO
-settings ns = Settings
+settings :: Settings Loader
+settings = Settings
   { historyFile    = Nothing
-  , complete       = completeWord Nothing " \t" $ return . searchFunc ns
+  , complete       = completeWord Nothing " \t" $ \str -> do
+      ns <- names <$> get
+      return (searchFunc ns str)
   , autoAddHistory = True }
 
 main :: IO ()
 main = do
   args <- getArgs
+  putStrLn welcome
   case getOpt Permute options args of
     (flags,files,[])
       | Help    `elem` flags -> putStrLn $ usageInfo usage options
       | otherwise -> case files of
        []  -> do
-         putStrLn welcome
-         runInputT (settings []) (loop flags "" [] (TC.emptyEnv []))
+         return ()
        [f] -> do
-         putStrLn welcome
          putStrLn $ "Loading " ++ show f
-         initLoop flags (dropFileName f) (dropExtension (takeFileName f))
+         let p = initLoop flags (dropFileName f) (dropExtension (takeFileName f))
+         _ <- runStateT (runInputT settings p) initState
+         return ()
        _   -> putStrLn $ "Input error: zero or one file expected\n\n" ++
                          usageInfo usage options
     (_,_,errs) -> putStrLn $ "Input error: " ++ concat errs ++ "\n" ++
                              usageInfo usage options
 
 -- Initialize the main loop
-initLoop :: [Flag] -> FilePath -> FilePath -> IO ()
+initLoop :: [Flag] -> FilePath -> FilePath -> Interpreter ()
 initLoop flags prefix f = do
   -- Parse and type-check files
-  (res,ms) <- runStateT (load prefix f) []
-  let k ns e = runInputT (settings ns) (loop flags prefix f (e (TC.emptyEnv ms)))
+  res <- lift (load prefix f)
   case res of
     C.Failed err -> do
-      putStrLn $ render $ sep ["Loading failed:",err]
-      k [] id
+      outputStrLn $ render $ sep ["Loading failed:",err]
     C.Loaded v t -> do
-      putStrLn "File loaded."
-      go v t k
+      outputStrLn "File loaded."
+      go v t
+  loop flags prefix f
 
-go :: C.Val -> C.Val -> ([C.Ident] -> (TC.TEnv -> TC.TEnv) -> IO b) -> IO b
-go v (C.VRecordT atele) k = k [n | (n,_) <- C.teleBinders atele]
-                              (TC.addDecls (E.etaExpandRecord atele v,atele))
-go v (C.VPi x _rig _a b) k = do
-  putStrLn $ "Parametric module: entering with abtract parameters"
-  go (E.app v (C.VVar x)) (E.app b (C.VVar x)) k
-go _ t k = do putStrLn $ "Module does not have a record type, but instead:\n" ++ show t
-              k [] id
-      -- Compute names for auto completion
+-- setTcEnv :: MonadState InterpState m => [String] -> (TC.TEnv -> TC.TEnv) -> m ()
+setTcEnv ns mk = modify (\st -> st {mkEnv = mk, names = ns})
+
+go :: C.Val -> C.Val -> Interpreter ()
+go v (C.VRecordT atele) = lift $ setTcEnv [n | (n,_) <- C.teleBinders atele]
+                                          (TC.addDecls (E.etaExpandRecord atele v,atele))
+go v (C.VPi x _rig _a b) = do
+  outputStrLn $ "Parametric module: entering with abtract parameters"
+  go (E.app v (C.VVar x)) (E.app b (C.VVar x))
+go _ t = outputStrLn $ "Module does not have a record type, but instead:\n" ++ show t
+
 
 -- The main loop
-loop :: [Flag] -> FilePath -> FilePath -> TC.TEnv -> Interpreter ()
-loop flags prefix f tenv@(TC.TEnv _ _rho _ _ _ms) = do
-  let cont = loop flags prefix f tenv
+loop :: [Flag] -> FilePath -> FilePath -> Interpreter ()
+loop flags prefix f = do
+  let cont = loop flags prefix f
   input <- getInputLine prompt
   case input of
     Nothing    -> outputStrLn help >> cont
     Just ":q"  -> return ()
-    Just ":r"  -> lift $ initLoop flags prefix f
+    Just ":r"  -> initLoop flags prefix f
     Just (':':'l':' ':str)
       | ' ' `elem` str -> do outputStrLn "Only one file allowed after :l"
                              cont
-      | otherwise      -> lift $ initLoop flags prefix str
-    Just (':':'c':'d':' ':str) -> do lift (setCurrentDirectory str)
+      | otherwise      -> initLoop flags prefix str
+    Just (':':'c':'d':' ':str) -> do liftIO (setCurrentDirectory str)
                                      cont
     Just ":h"  -> outputStrLn help >> cont
     Just str   -> do
@@ -123,7 +137,8 @@ loop flags prefix f tenv@(TC.TEnv _ _rho _ _ _ms) = do
           case runResolver {- FIXME -} "<interactive>" $ resolveExp expr of
             Left  err  -> outputStrLn (render ("Resolver failed:" </> err))
             Right (body,(),_imports_ignored_here) -> do
-            let (x,msgs) = TC.runInfer tenv body
+            e <- lift get
+            let (x,msgs) = TC.runInfer (mkEnv e (TC.emptyEnv (modules e))) body
             forM_ msgs $ outputStrLn . render
             case x of
               Left err -> do outputStrLn (render ("Could not type-check:" </> err))
@@ -133,10 +148,10 @@ loop flags prefix f tenv@(TC.TEnv _ _rho _ _ _ms) = do
       cont
 
 
-load :: String -> FilePath -> StateT C.Modules IO C.ModuleState
+load :: String -> FilePath -> Loader C.ModuleState
 load prefix f = do
   liftIO $ putStrLn $ "Loading: " ++ f
-  (ms::C.Modules) <- get :: StateT C.Modules IO C.Modules
+  ms <- modules <$> get
   case lookup f ms of
     Just C.Loading -> return $ C.Failed "cycle in imports"
     Just r@(C.Loaded _ _) -> return r
@@ -157,11 +172,11 @@ load prefix f = do
                   Left err -> return $ C.Failed $ sep ["Resolver error:", err]
                   Right (t,_,imps) -> do
                     forM_ imps (load prefix)
-                    ms' <- get
+                    ms' <- modules <$> get
                     let (x,msgs) = TC.runModule (TC.emptyEnv ms') t
                     liftIO $ forM_ msgs $ putStrLn . render
                     return x
-      modify ((f,res):)
+      modify (\e -> e {modules = (f,res):modules e})
       return res
 
 help :: String
